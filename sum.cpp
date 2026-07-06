77bd962befa0d1d19a641495a71472f9167ab5ba
teddy <haribozyme@korea.ac.kr>
2026-07-06 15:15:45 +0900

    O(n) -> O(1)

--- sum.cpp
+++ sum.cpp
@@ -2,9 +2,9 @@
 #include "sum.h"
 
 int sum(int n) {
-	int res = 0;
-	for (int i = 1; i <= n; ++i)
-		res += i;
+	int res = ((n+1) * n) / 2;
+/*	for (int i = 1; i <= n; ++i)
+		res += i;*/
 	return res;
 }
 
